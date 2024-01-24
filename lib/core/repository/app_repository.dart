96d9ffba96499c5469d73f4567388484e993af22
Felmeta-M <felmetamuktar23@gmail.com>
2024-01-24 20:04:01 +0300

    adding pages

---
+++ lib/core/repository/app_repository.dart
@@ -0,0 +1,8 @@
+
+abstract class IAppRepository {
+  // final AppDataProviderSingleton _appDataProviderSingleton;
+  // AppRepository._(
+  //   this._appDataProviderSingleton,
+  // );
+
+}
---
+++ lib/features/property/bloc/Reviews/reviews_event.dart
@@ -0,0 +1,25 @@
+import 'package:equatable/equatable.dart';
+
+abstract class ReviewsEvent extends Equatable {
+  const ReviewsEvent();
+}
+
+class ReviewsLoadStarted extends ReviewsEvent {
+  const ReviewsLoadStarted();
+
+  @override
+  List<Object> get props => [];
+}
+
+class ReviewsLoaded extends ReviewsEvent {
+  final List<dynamic> reviews;
+  const ReviewsLoaded(this.reviews);
+
+  @override
+  List<Object> get props => [reviews];
+}
+
+class ReviewsLoadingFailed extends ReviewsEvent {
+  @override
+  List<Object> get props => [];
+}
